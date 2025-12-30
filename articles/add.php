812ef9638f0a72ea7bb2364bb409fe20137ebcae
Faizpi <faizbintang1244@gmail.com>
2025-12-30 21:48:46 +0700

    Fix: Use UPLOAD_PATH_ARTICLES for article images

--- articles/add.php
+++ articles/add.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Handle image upload
     $imageName = null;
     if (!empty($_FILES['image']['name'])) {
-        $uploadDir = UPLOAD_PATH . 'articles/';
+        $uploadDir = UPLOAD_PATH_ARTICLES;
         if (!file_exists($uploadDir)) {
             mkdir($uploadDir, 0755, true);
         }
--- articles/edit.php
+++ articles/edit.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Handle image upload
     $newImage = null;
     if (!empty($_FILES['image']['name'])) {
-        $uploadDir = UPLOAD_PATH . 'articles/';
+        $uploadDir = UPLOAD_PATH_ARTICLES;
         if (!file_exists($uploadDir)) {
             mkdir($uploadDir, 0755, true);
         }
